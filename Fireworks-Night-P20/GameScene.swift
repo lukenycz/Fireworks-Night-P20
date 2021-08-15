6fad4f1b38df1721a723a0c68999b5ebbff7b266
Łukasz Nycz <lnycz96@gmail.com>
2021-08-15 11:04:35 +0200

    create firework

--- Fireworks-Night-P20/GameScene.swift
+++ Fireworks-Night-P20/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 class GameScene: SKScene {
 
     var gameTimer: Timer?
-    var fireworks: [SKNode]()
+    var fireworks = [SKNode]()
     
     var leftEdge = -22
     var bottomEdge = -22
@@ -31,5 +31,33 @@ class GameScene: SKScene {
         addChild(background)
         
         gameTimer =  Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
+    }
+    
+    @objc func launchFireworks(xMovement: CGFloat, x: Int, y: Int) {
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch Int.random(in: 0...2) {
+        case 0:
+            firework.color = .cyan
+        case 1:
+            firework.color = .green
+        default:
+            firework.color = .red
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: xMovement, y: 1000))
+        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+        
+        node.run(move)
+        
+        
     }
 }
